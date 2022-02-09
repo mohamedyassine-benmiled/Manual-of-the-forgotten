705b6a45f9f94c97e057e12750c90c5e638a53c5
mohamedyassine-benmiled <mohamedyassine.benmiled@esprit.tn>
2022-02-09 11:45:36 +0100

    Added Menu Buttons

--- Game/main.c
+++ Game/main.c
@@ -7,19 +7,23 @@ int main (int argc , char *argv[])
 {
  SDL_Surface *ecran = NULL ; 
  SDL_Surface *menu = NULL ; 
-
+ SDL_Surface *button1 = NULL ; 
  SDL_Rect positionMenu ; 
+ SDL_Rect positionButton1 ; 
  int cont = 3 ; 
  SDL_Event event ; 
 
  SDL_Init(SDL_INIT_VIDEO);
-        SDL_WM_SetIcon(IMG_Load("Logo.png"),NULL);
-        ecran = SDL_SetVideoMode(1920,1080,32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-        SDL_WM_SetCaption("Hello world",NULL);
+        SDL_WM_SetIcon(IMG_Load("Menu/Logo.png"),NULL);
+        ecran = SDL_SetVideoMode(1920,1080,32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_FULLSCREEN);
+        SDL_WM_SetCaption("Manual of the forgotten",NULL);
 
 menu = IMG_Load("Menu/Game_Background.png");
 positionMenu.x=0;
 positionMenu.y=0;
+button1 = IMG_Load("Menu/Play.png");
+positionButton1.x=129;
+positionButton1.y=374;
 while(cont)
 {
     SDL_WaitEvent(&event);
@@ -29,11 +33,18 @@ while(cont)
         cont=0; 
         break;
      case SDL_KEYDOWN:
+ 
      switch (event.key.keysym.sym)
      {
-     case SDLK_ESCAPE:
-         cont = 0 ; 
+       case (SDLK_ESCAPE):
+         cont = 0; 
          break;
+       case (SDLK_f):
+        cont = 1; 
+        break ;
+       case (SDLK_o):
+        cont=2; 
+        break ; 
      }
     
 
@@ -45,11 +56,13 @@ while(cont)
 
 
 SDL_BlitSurface(menu,NULL,ecran,&positionMenu);
+SDL_BlitSurface(button1,NULL,ecran,&positionButton1);
 SDL_Flip(ecran);
 
 
 }
 SDL_FreeSurface(menu);
+SDL_FreeSurface(button1);
 SDL_Quit();
 return EXIT_SUCCESS;
 
