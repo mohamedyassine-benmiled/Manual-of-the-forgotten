f3355ad048fbe46576e316cc91714f2b5f9a2a5b
ilyeschaabani <97950468+ilyeschaabani@users.noreply.github.com>
2022-02-16 11:44:47 -0500

    hehehe

---
+++ game/animation.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <string.h>
+
+void animation ()
+{
+     SDL_Surface *anim[100] ;
+    SDL_Rect position_anim = {0,0,0}
+
+
+
+
+
+}
+
+
