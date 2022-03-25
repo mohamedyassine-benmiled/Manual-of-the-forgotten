a5d3ec45737ce65c12f38c838743afd3a3f8e4ea
mohamedyassine-benmiled <mohamedyassine.benmiled@esprit.tn>
2022-03-25 11:30:20 +0100

    Updated some structures

--- game/include/game.h
+++ game/include/game.h
@@ -6,9 +6,23 @@
 #include "init.h"
 #include "menu.h"
 #include "animation.h"
+#include "collision.h"
 
+typedef enum Behavior
+{
+PATROL, FOLLOW, ATTACK, REFRACT
+}Behavior;
 
-
+typedef struct Character
+{
+    SDL_Surface *image;
+    SDL_Rect pos, src_pos, relativePos;
+    Cercle pos_cercle;
+    int life, died;
+    int elapsed;
+    int direction, speed;
+    Behavior behavior;
+}Character;
 
 
 int game(SDL_Surface *screen,int run);
