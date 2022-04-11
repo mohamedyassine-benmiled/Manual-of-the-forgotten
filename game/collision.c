0fce82314cf056b0102af03c8f3f65efd0645708
mohamedyassine-benmiled <mohamedyassine.benmiled@esprit.tn>
2022-04-11 21:48:08 +0100

    Fixing Collision for wael

--- game/collision.c
+++ game/collision.c
@@ -12,29 +12,30 @@ SDL_Color getpixel(SDL_Surface *pSurface,int x,int y)
 	SDL_GetRGB(col,pSurface->format, &color.r, &color.g, &color.b);
 	return (color);
 }
-int  collisionparfaite(SDL_Surface *psurface,Game p)
+
+int  collisionparfaite(SDL_Surface *psurface,Character player)
 {	
 	int tabx[7],taby[7],i,collision=0;
 	SDL_Color color1,color;
 	color1.r = 0;
 	color1.g = 0;
 	color1.b = 0;
-	tabx[0]=p.player->position.x;
-	tabx[1]=(p.player->position.x)+((p.player->position.w)/2);
-	tabx[2]=(p.player->position.x+p.player->position.w);
-	tabx[3]=p.player->position.x;
-	tabx[4]=p.player->position.x;
-	tabx[5]=(p.player->position.x)+((p.player->position.w)/2);
-	tabx[6]=(p.player->position.x+p.player->position.w);
-	tabx[7]=(p.player->position.x+p.player->position.w);
-	taby[0]=p.player->position.y;
-	taby[1]=p.player->position.y;
-	taby[2]=p.player->position.y;
-	taby[3]=(p.player->position.y)+((p.player->position.h)/2);
-	taby[4]=(p.player->position.y+p.player->position.h);
-	taby[5]=(p.player->position.y+p.player->position.h);
-	taby[6]=(p.player->position.y+p.player->position.h);
-	taby[7]=(p.player->position.y)+((p.player->position.h)/2);
+	tabx[0]=player.position.x;
+	tabx[1]=(player.position.x)+((player.position.w)/2);
+	tabx[2]=(player.position.x+player.position.w);
+	tabx[3]=player.position.x;
+	tabx[4]=player.position.x;
+	tabx[5]=(player.position.x)+((player.position.w)/2);
+	tabx[6]=(player.position.x+player.position.w);
+	tabx[7]=(player.position.x+player.position.w);
+	taby[0]=player.position.y;
+	taby[1]=player.position.y;
+	taby[2]=player.position.y;
+	taby[3]=(player.position.y)+((player.position.h)/2);
+	taby[4]=(player.position.y+player.position.h);
+	taby[5]=(player.position.y+player.position.h);
+	taby[6]=(player.position.y+player.position.h);
+	taby[7]=(player.position.y)+((player.position.h)/2);
 	
 	for(i=0;i<8;i++)
 	{
