39a3936768ecc72f7dc7b415a92740e35fe525f2
ilyeschaabani <97950468+ilyeschaabani@users.noreply.github.com>
2022-04-11 15:15:17 -0400

    enemy

---
+++ game/enemy.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "include/game.h"
+#include "enemy.h"
+
+
+int spotted(Game *game, int i)
+{
+    return ((game->player->position.x+game->bg.scroll_x < game->enemy->position.x && game->enemy.direction == -1) || (game->player.position.x+game->bg.scroll_x > game->enemy.position.x && game->enemy->direction == 1) ) ;
+}
+void movement_enemy(Game *game, int n)
+{
+
+        if (game->enemy.behavior==PATROL || game->enemy.behavior==REFRACT)
+        {
+            if (!game->enemy[n])
+            {
+                game->enemy.=SDL_GetTicks();
+                game->npc[n].fix=1;
+            }
+
+
+            game->enemy[n].position.x+=game->enemy[n].speed*game->enemy[n].direction;
+
+            if (SDL_TICKS_PASSED(game->enemy.elapsed, 5000))
+            {
+                game->enemy[n].direction*=(-1);
+                game->enemy[n].fix=0;
+            }
+
+            if (game->enemy[n].direction==1)
+                game->enemy[n].look=0;
+
+            else
+                game->enemy[n].look=1;
+
+            game->enemy[n].spritestate++;
+            if (game->enemy[n].spritestate>4)
+                game->enemy[n].spritestate=0;
+        }
+
+        else if (game->enemy[n].behavior==ATTACK)
+        {
+            if (game->enemy[0].direction==1)
+            {
+                game->enemy[0].look=2;
+                game->enemy[0].spritestate=0;
+            }
+            else
+            {
+                game->enemy[0].look=2;
+                game->enemy[0].spritestate=1;
+            }
+
+	
+
+        }
+
+
+}
