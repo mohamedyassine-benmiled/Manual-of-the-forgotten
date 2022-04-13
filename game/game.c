c7414f59d00e3c4002141cb9ac6300f814caac20
mohamedyassine-benmiled <mohamedyassine.benmiled@esprit.tn>
2022-04-13 13:58:06 +0100

    Push

---
+++ game/game.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "include/game.h"
+
+
+int game(SDL_Surface *screen,int run)
+{
+    Game g;
+    initbackground(&g.bg);
+    initplayer(&g);
+    SDL_Event event;
+    showgame(g.bg.img,screen);
+    SDL_Flip(screen);
+    SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY,SDL_DEFAULT_REPEAT_INTERVAL);
+    while(run==3)
+    {
+
+    //Wait for event
+    while (SDL_PollEvent(&event))
+    {
+
+    switch (event.type)
+        {
+        case SDL_QUIT:
+            run=0;
+            break;
+                case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                    {
+                        case SDLK_RIGHT:
+                        {
+                            g.player[0].input.right=1;
+                            g.player[0].input.left=0;
+                        }
+                        break;
+
+                        case SDLK_LEFT:
+                        {
+                            g.player[0].input.left=1;
+                            g.player[0].input.right=0;
+                        }
+                        break;
+
+                        case SDLK_UP:
+                        {
+                            g.player[0].input.up=1;
+                        }
+                        break;
+                    //On Escape Press : Quit Check
+                    case (SDLK_ESCAPE):
+                        if (check(screen,&run,0))
+                           run=0; 
+                           else
+                           {
+                                showgame(g.bg.img,screen);
+                           }  
+                    break;
+                    //On "m" Press : Go back to Menu
+                    case (SDLK_m):
+                        run=1;
+                    break;
+                    //On "o" press : Go to option
+                    case (SDLK_o):
+                        run=2;
+                    break;
+                    //On "p" press : Go to game
+                    case (SDLK_p):
+                        run=3;
+                    break;
+                    default:
+                    break;
+                    }
+                break;
+                case SDL_KEYUP:
+                switch (event.key.keysym.sym)
+                    {
+                        case SDLK_RIGHT:
+                        {
+                            g.player[0].input.right=0;
+                        }
+                        break;
+
+                        case SDLK_LEFT:
+                        {
+                            g.player[0].input.left=0;
+                        }
+                        break;
+
+                        case SDLK_UP:
+                        {
+                            g.player[0].input.up=0;
+                        }
+                        break;
+
+                    default:
+                    break;
+                    }
+                break;
+
+        }
+        
+        
+    }
+    movement(&g);
+    gamerefresh(&g,screen);
+    }
+    freebackground(g.bg);
+return run;
+
+}
+
+
---
+++ game/image.c
@@ -0,0 +1,212 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "include/image.h"
+#include "include/game.h"
+#include "include/menu.h"
+#include "include/declarations.h"
+#include "include/config.h"
+
+//Showing Game Images in the right position
+void gamerefresh(Game *g,SDL_Surface *screen)
+{
+    settings config;
+    get_config(&config);
+    unsigned int elapsed;
+    unsigned int lasttime = SDL_GetTicks();
+
+    //scrolling(g);
+
+        g->player[0].src_pos.x=CHAR_W*g->player[0].spritestate;
+        g->player[0].src_pos.y=CHAR_H*g->player[0].look;
+        g->player[0].src_pos.h=CHAR_H;
+        g->player[0].src_pos.w=CHAR_W;
+ 
+    showgame(g->bg.img,screen);
+  SDL_BlitSurface(g->player[0].image,&g->player[0].src_pos,screen,&g->player[0].position);
+    /* Fixing fps */
+    elapsed = SDL_GetTicks()-lasttime;
+    if (elapsed<1000/FPS)
+        SDL_Delay(1000/FPS-elapsed);
+
+
+
+    SDL_Flip(screen);
+}
+//Showing Graphics Images to refresh screen
+void graphicsrefresh(graphicimage *assets,SDL_Surface *screen,int fullscreen)
+{
+    show(assets->boxresolution,screen);
+    show(assets->selectresolution,screen);
+    show(assets->windowsettings,screen);
+
+    if (fullscreen)
+    {
+        show(assets->firstbox[0],screen);
+        show(assets->secondbox[1],screen);
+    }
+    else
+    {
+        show(assets->firstbox[1],screen);
+        show(assets->secondbox[0],screen);
+    }
+
+    show(assets->fullscreen,screen);
+    show(assets->windowed,screen);
+    show(assets->currentresolution,screen);
+}
+//Showing Menu Images to refresh screen
+void menurefresh(MenuImage *assets,SDL_Surface *screen)
+{
+        show(assets->background,screen);
+        show(assets->play[0],screen);
+        show(assets->options[0],screen);
+        show(assets->quit[0],screen);
+        show(assets->logogroup,screen);
+        show(assets->copyright,screen);
+}
+//Showing Option Images to refresh screen
+void optionrefresh(OptionImage *assets,SDL_Surface *screen)
+{
+    show(assets->background,screen);
+    show(assets->logogroup,screen); 
+    show(assets->obook[14],screen);
+    show(assets->arrow[0],screen);
+    show (assets->graphics[0],screen);
+    show (assets->audio[0],screen);
+    show (assets->keybinds[0],screen);
+
+}
+//Showing Audio Images to refresh screen
+void audiorefresh(audioimage *assets,SDL_Surface *screen,int audio)
+{
+    show(assets->volume,screen);
+    show(assets->audio,screen);
+    show(assets->on,screen);
+    show(assets->off,screen);
+    show(assets->minus,screen);
+    show(assets->plus,screen);
+    show(assets->audiobar,screen);
+    if (audio)
+    {
+        show(assets->onbox[0],screen);
+        show(assets->offbox[1],screen);
+    }
+    else
+    {
+        show(assets->onbox[1],screen);
+        show(assets->offbox[0],screen);
+    }
+    
+}
+//Freeing Menu Images from memory
+void freemenu(MenuImage assets)
+{
+SDL_FreeSurface(assets.play[0].surface);
+SDL_FreeSurface(assets.options[0].surface);
+SDL_FreeSurface(assets.quit[0].surface);
+SDL_FreeSurface(assets.play[1].surface);
+SDL_FreeSurface(assets.options[1].surface);
+SDL_FreeSurface(assets.quit[1].surface);
+SDL_FreeSurface(assets.background.surface);
+SDL_FreeSurface(assets.logogroup.surface);
+SDL_FreeSurface(assets.copyright.surface);
+for (int i = 0; i < 4; i++)
+{
+    SDL_FreeSurface(assets.logo[i].surface);
+}
+for (int i = 0; i < 13; i++)
+{
+    SDL_FreeSurface(assets.cbook[i].surface);
+}
+}
+//Freeing Option Images from memory
+void freeoption(OptionImage assets)
+{
+    for (int i = 0; i < 15; i++)
+    {
+    SDL_FreeSurface(assets.obook[i].surface);
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        SDL_FreeSurface(assets.graphics[i].surface);
+                SDL_FreeSurface(assets.audio[i].surface);
+                        SDL_FreeSurface(assets.keybinds[i].surface);
+                            SDL_FreeSurface(assets.arrow[i].surface);
+    }
+SDL_FreeSurface(assets.background.surface);
+SDL_FreeSurface(assets.logogroup.surface);
+}
+//Freeing Graphics Images from memory
+void freegraphics(graphicimage assets)
+{
+
+    for (int i = 0; i < 2; i++)
+    {
+        SDL_FreeSurface(assets.firstbox[i].surface);
+                SDL_FreeSurface(assets.secondbox[i].surface);
+                        SDL_FreeSurface(assets.listresolutionbox[i].surface);
+    }
+
+SDL_FreeSurface(assets.selectresolution.surface);
+SDL_FreeSurface(assets.windowsettings.surface);
+SDL_FreeSurface(assets.fullscreen.surface);
+SDL_FreeSurface(assets.windowed.surface);
+SDL_FreeSurface(assets.boxresolution.surface);
+SDL_FreeSurface(assets.currentresolution.surface);
+SDL_FreeSurface(assets.listresolution.surface);
+
+}
+//Freeing Audio Images from memory
+void freeaudio(audioimage assets)
+{
+
+    for (int i = 0; i < 2; i++)
+    {
+        SDL_FreeSurface(assets.onbox[i].surface);
+                SDL_FreeSurface(assets.offbox[i].surface);
+                        
+    }
+    for (int i = 0; i < 10; i++)
+    {SDL_FreeSurface(assets.circle[i].surface);
+    }
+    
+
+SDL_FreeSurface(assets.volume.surface);
+SDL_FreeSurface(assets.audio.surface);
+SDL_FreeSurface(assets.on.surface);
+SDL_FreeSurface(assets.off.surface);
+SDL_FreeSurface(assets.minus.surface);
+SDL_FreeSurface(assets.plus.surface);
+SDL_FreeSurface(assets.audiobar.surface);
+}
+//Freeing Check Images from memory
+void freecheck(CheckImage assets)
+{
+SDL_FreeSurface(assets.Window.surface);
+SDL_FreeSurface(assets.Apply.surface);
+SDL_FreeSurface(assets.Quit.surface);
+for (int i = 0; i < 2; i++)
+{
+
+
+SDL_FreeSurface(assets.No[i].surface);
+SDL_FreeSurface(assets.Yes[i].surface);
+}
+}
+//Freeing Background Images from memory
+void freebackground(Background assets)
+{
+    SDL_FreeSurface(assets.img.surface);
+}
+//Showing an image on the screen
+void show(image p,SDL_Surface *screen)
+{
+    SDL_BlitSurface(p.surface,NULL,screen,&p.pos1);
+}
+//Showing Characters and Enemies in game
+void showgame(image p,SDL_Surface *screen)
+{
+    SDL_BlitSurface(p.surface,&p.pos2,screen,&p.pos1);
+}
