60301828f9736a2f861a5467747d4b47eb470396
Raedbedoui <99202274+Raedbedoui@users.noreply.github.com>
2022-04-13 10:18:54 -0400

    added files

---
+++ game/include/minimap.h
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef struct
+{
+    image bg;
+    image player[4];
+    image enemy[10];
+    int scroll_x;
+    int scroll_y;
+    int level;
+} Minimap;
+
+
+
+
+
+
+
