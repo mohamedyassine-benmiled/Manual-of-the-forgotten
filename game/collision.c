5dab5445348e4e8ce5743145907cfb0e14a39c12
wjbwastaken1 <wael.bechraoui@esprit.tn>
2022-04-14 04:31:37 +0200

    Update collision.c

--- game/collision.c
+++ game/collision.c
@@ -113,8 +113,42 @@ int collision_box(Box b1, Box b2)
 {
     return(b1.x > b2.x-8 && b1.x < b2.x+8);
 }
+/*
+int Collision_PPP(SDL_Surface *psurface,Character player)
+{	
+int tabx[7],taby[7],i,collision=0;
+	SDL_Color color1,color;
+	color1.r = 255;
+	color1.g = 255;
+	color1.b = 255;
+	tabx[0]=player.position.x;
+	tabx[1]=(player.position.x)+((player.position.w)/2);
+	tabx[2]=(player.position.x+player.position.w);
+	tabx[3]=player.position.x;
+	tabx[4]=player.position.x;
+	tabx[5]=(player.position.x)+((player.position.w)/2);
+	tabx[6]=(player.position.x+player.position.w);
+	tabx[7]=(player.position.x+player.position.w);
+	taby[0]=player.position.y;
+	taby[1]=player.position.y;
+	taby[2]=player.position.y;
+	taby[3]=(player.position.y)+((player.position.h)/2);
+	taby[4]=(player.position.y+player.position.h);
+	taby[5]=(player.position.y+player.position.h);
+	taby[6]=(player.position.y+player.position.h);
+	taby[7]=(player.position.y)+((player.position.h)/2);
+	
+	for(i=0;i<8;i++)
+	{
+	color=getpixel(psurface,tabx[i],taby[i]);
+	if(color.r==color1.r && color.b==color1.b && color.g==color1.g)
+	{
+		collision=1;
 
-
-
-
+	}
+	}
+	return collision;
+	
+}	
+*/
 
