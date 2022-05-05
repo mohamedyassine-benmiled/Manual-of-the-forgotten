46048b8ee1412889006e2797a787d0d105c73045
ilyeschaabani <97950468+ilyeschaabani@users.noreply.github.com>
2022-05-05 18:15:20 -0400

    init

--- game/enigme.c
+++ game/enigme.c
@@ -18,58 +18,64 @@
 #include "include/enigme.h"
 #include "include/text.h"
 
-void init_enigme(enigme *e )
+void init_enigme(Enigme *e )
 {
-    text t;
+FILE *f=NULL;
+text t1, t2 ;
+char logo [50];
+int check=0;
+int alea;
+int nbligne=0;
+int reponse;
+
+
 //text color
 t.textColor.r=0;
 t.textColor.g=0;
 t.textColor.b=0;
-TTF_Font *fontquest;
-int check=0;
-int alea;
-int nbligne=0;
 
-int reponse;
-FILE *f=NULL;
+
 f=fopen("enigmes","r");
+
+srand(time(NULL));
+alea=rand()%4;
 if(f!=NULL)
 {
 while(fscanf(f,"%s %s %s %s %d\n",e->question,e->rep1,e->rep2,e->rep3,&reponse)!=EOF)
 {
 nbligne++;
+if (nbligne == alea)
+{
+    break;
+}
+
 }
 fclose(f);
 }
-srand(time(NULL));
-alea=rand()%4;
-f=fopen("enigme","r");
-if(f!=NULL)
-{
-while(fscanf(f,"%s %s %s %s %d\n",e->question,e->rep1,e->rep2,e->rep3)!=EOF)
-{
-check++;
-if(check==alea){break;}
-}
-fclose(f);
-}
-//Affichage
-e->posquestion.x=50;
-e->posquestion.y=20;
-e->posreponse1.x=400;
-e->posreponse1.y=50;
-e->posreponse2.x=400;
-e->posreponse2.y=100;
-e->posreponse3.x=400;
-e->posreponse3.y=150;
-fontquest=TTF_OpenFont ("Trajan Pro.ttf",30);
-t.font=TTF_OpenFont("Trajan Pro.ttf",20);
+
+e->q.pos1.x=0;
+e->q.pos1.y=0;
+e->r1.pos1.x=0;
+e->r1.pos1.y=0;
+e->r2.pos1.x=0;
+e->r2.pos1.y=0;
+e->r3.pos1.x=0;
+e->r3.pos1.y=0;
+t1.font=TTF_OpenFont ("alagard.ttf",30);
+t2.font=TTF_OpenFont("alagard.ttf",20);
 SDL_Color black={0,0,0};
 SDL_Color white={255,255,255};
-e->surfacequestion=TTF_RenderText_Blended(fontquest,e->question,black);
-e->surfacereponse1=TTF_RenderText_Blended(t.font,e->rep1,white);
-e->surfacereponse2=TTF_RenderText_Blended(t.font,e->rep2,white);
-e->surfacereponse3=TTF_RenderText_Blended(t.font,e->rep3,white);
+e->q.surface=TTF_RenderText_Blended(t2.font,e->q,black);
+e->r1.surface=TTF_RenderText_Blended(t1.font,e->r1,white);
+e->r2.surface=TTF_RenderText_Blended(t1.font,e->r2,white);
+e->r3.surface=TTF_RenderText_Blended(t1.font,e->r3,white);
+ for(int i=0;i<4;i++)
+    {
+    sprintf(logo,"graphics/1080/Logo/Logo%d.png",i);
+    assets->logo[i].surface=IMG_Load(logo);
+    assets->logo[i].pos1.x=619;
+    assets->logo[i].pos1.y=47;
+    }
 }
 //Score
 void Game_Score (image *s,int *score,int response)
